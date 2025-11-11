c74728b6bdf022fd05bd351ecf377aeaebadcb85
Lucasesgomes <lucasgomesferreira2008@gmail.com>
2025-11-11 16:58:29 -0300

    delayManagement + users add - userRow/userList made integration with api + axios (missing bearear token api)

--- client/src/components/layout/userRow.jsx
+++ client/src/components/layout/userRow.jsx
@@ -1,30 +1,20 @@
-//import { useEffect } from 'react'
-//import { Clicpboard-list, X } from 'lucide-react';
+const UserRow = ({ user }) => {
+  return (
+    <div className="user-row border p-3 rounded-lg shadow bg-white flex flex-col gap-1">
+        <div>
+            <span><strong>Nome:</strong> {user.nome}</span>
+        </div>
+        <div>
+            <span><strong>Email:</strong> {user.email || "—"}</span>
+        </div>
+        <div>
+            <span><strong>CPF:</strong> {user.cpf || "—"}</span>
+        </div>
+        <div>
+            <span><strong>Cargo:</strong> {user.cargo}</span>
+        </div>
+    </div>
+  );
+};
 
-//Status colors
-/*const statusColors = {
-    validado: 'bg-green-200 text-green-800',
-    pendente: 'bg-yellow-200 text-yellow-800',
-    expirado: 'bg-red-200 text-red-800',
-}*/
-
-const userRow = (
-    nome, email, id, matricula, status, rg, foto
-) => {
-
-    return (
-        <>
-            <div className="flex items-center justify-between p-4 border-gray-400">
-                {/* Usuário */}
-                <div className="flex items-center gap-3 w-[25%]">
-                    <img src={foto} alt="" />
-
-                </div>
-
-            </div>
-            
-        </>
-    );
-}
-
-export default userRow;
+export default UserRow;
--- client/src/pages/delayControl.jsx
+++ client/src/pages/delayControl.jsx
@@ -4,7 +4,9 @@ import { NavLink } from "react-router-dom";
 import FormManagement from "../components/containers/formManagement.jsx";
 import LoggedHeader from "../components/layout/loggedHeader.jsx"
 import MainContent from '../components/layout/mainContent.jsx'
+import UserList from "../components/layout/userList.jsx";
 import Footer from "../components/layout/footer.jsx"
+
 
 //icons
 import { AlarmClock } from "lucide-react";
@@ -21,6 +23,7 @@ function DelayControl() {
                     title="Gerenciamento de Atrasos"
                     bgColor="bg-red-500"
                 >
+                    <UserList />
                 </FormManagement>
             </MainContent>
 
