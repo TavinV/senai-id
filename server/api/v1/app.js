d8a8dd7ce9ab64a475978c062459cf8789ab704c
TavinV <otavioviniciusads@gmail.com>
2025-11-29 16:46:16 -0300

    Configure CORS middleware to specify allowed origins and methods

--- server/api/v1/app.js
+++ server/api/v1/app.js
@@ -22,7 +22,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }
+))
 
 // === Rotas ===
 import auth_routes from './routes/auth_routes.js'
