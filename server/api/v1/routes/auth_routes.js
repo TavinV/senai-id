641f0d692dbc0a9ff2d484768838f2970d49ea1e
TavinV <otavioviniciusads@gmail.com>
2025-11-29 13:55:30 -0300

    Increase login request rate limit window to 50 minutes

--- server/api/v1/routes/auth_routes.js
+++ server/api/v1/routes/auth_routes.js
@@ -5,7 +5,7 @@ import rateLimit from 'express-rate-limit';
 const router = express.Router()
 
 const loginLimiter = rateLimit({
-    windowMs: 5 * 60 * 1000, // 5 minutes
+    windowMs: 50 * 60 * 1000, // 5 minutes
     max: 5, // Limit each IP to 5 login requests per `window` (here, per 5 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
