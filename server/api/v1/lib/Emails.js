39c752aef1ce8b94d0f9df4222b18b05c63de556
TavinV <otavioviniciusads@gmail.com>
2025-12-01 22:24:07 -0300

    refactor: comment out email sending logic in Emails.js

--- server/api/v1/lib/Emails.js
+++ server/api/v1/lib/Emails.js
@@ -3,31 +3,31 @@ const NOREPLY_EMAIL = process.env.NOREPLY_EMAIL
 const NOREPLY_EMAIL_PASS = process.env.NOREPLY_EMAIL_PASS
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for port 465, false for other ports
-    auth: {
-        user: NOREPLY_EMAIL,
-        pass: NOREPLY_EMAIL_PASS,
-    },
+    // host: "smtp.gmail.com",
+    // port: 465,
+    // secure: true, // true for port 465, false for other ports
+    // auth: {
+    //     user: NOREPLY_EMAIL,
+    //     pass: NOREPLY_EMAIL_PASS,
+    // },
 });
 
 async function sendMail(to, subject, html) {
-    const mailInfo = {
-        from: NOREPLY_EMAIL,
-        to,
-        subject,
-        html
-    }
+    // const mailInfo = {
+    //     from: NOREPLY_EMAIL,
+    //     to,
+    //     subject,
+    //     html
+    // }
 
-    const emailDetails = await transporter.sendMail(mailInfo)
+    // const emailDetails = await transporter.sendMail(mailInfo)
 
-    if (emailDetails.accepted.length > 0) {
-        return [emailDetails, null]
-    } else {
+    // if (emailDetails.accepted.length > 0) {
+        return ["ok", null]
+    // } else {
 
-        return [null, "Não foi possível enviar o email."];
-    }
+    //     return [null, "Não foi possível enviar o email."];
+    // }
 }
 
 export default sendMail;
